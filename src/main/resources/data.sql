738f8695c13cf55f28f96aa867dce4c5eb881291
daniel <danielcolladomoreno@gmail.com>
2024-02-21 18:16:35 +0100

    wrapping up + sql data 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,18 +6,18 @@ CREATE TABLE superheroes (id INT AUTO_INCREMENT  PRIMARY KEY,
                           tier VARCHAR(100),
                           ranking int
                           );
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Spiderman', 'Peter', 'Parker', 'S', 7);
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Superman', 'Clark', 'Kent', 'S', 1);
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Batman', 'Bruce', 'Wayne', 'A', 3);
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Wonder Woman', 'Diana', 'Prince', 'B', 1);
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Iron Man', 'Tony', 'Stark', 'C', 8);
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Captain America', 'Steve', 'Rogers', 'S', 4);
-INSERT INTO superheroes
+INSERT INTO superheroes (alias, firstName, lastName, tier, ranking)
 VALUES ('Black Widow', 'Natasha', 'Romanoff', 'A', 1);
 
